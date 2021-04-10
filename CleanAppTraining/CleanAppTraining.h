5cc5010e976955cfdaa34a7b850ec65b21322979
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-09 23:06:20 -0300

    First commit

---
+++ CleanAppTraining/CleanAppTraining.h
@@ -0,0 +1,18 @@
+//
+//  CleanAppTraining.h
+//  CleanAppTraining
+//
+//  Created by Marcos Barbosa on 09/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanAppTraining.
+FOUNDATION_EXPORT double CleanAppTrainingVersionNumber;
+
+//! Project version string for CleanAppTraining.
+FOUNDATION_EXPORT const unsigned char CleanAppTrainingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanAppTraining/PublicHeader.h>
+
+
