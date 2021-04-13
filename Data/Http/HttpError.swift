fd0e5d02b9be544837eed1b3853fc7d586370a09
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-13 00:06:17 -0300

    Ensure RemoteAddAccount completes with error when HttpClient completes with error.

---
+++ Data/Http/HttpError.swift
@@ -0,0 +1,12 @@
+//
+//  HttpError.swift
+//  Data
+//
+//  Created by Marcos Barbosa on 13/04/21.
+//
+
+import Foundation
+
+public enum HttpError: Error {
+    case noConnectivity
+}
---
+++ Domain/Errors/DomainError.swift
@@ -0,0 +1,12 @@
+//
+//  DomainError.swift
+//  Domain
+//
+//  Created by Marcos Barbosa on 12/04/21.
+//
+
+import Foundation
+
+public enum DomainError: Error {
+    case unexpected
+}
