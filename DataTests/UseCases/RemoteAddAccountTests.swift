7e889b41d0ff54d61e83fe2b50c51509a177632b
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-14 00:10:25 -0300

    Add file and line to expected method.

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -60,7 +60,7 @@ extension RemoteAddAccountTests {
         return (sut, httpClientSpy)
     }
     
-    func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel?, DomainError>, when action: () -> Void) {
+    func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel?, DomainError>, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
         
         let exp = expectation(description: "waiting")
         sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
@@ -68,13 +68,13 @@ extension RemoteAddAccountTests {
             switch (expectedResult, receivedResult) {
             
             case (.failure(let expectedError), .failure(let receivedError)):
-                XCTAssertEqual(expectedError, receivedError)
+                XCTAssertEqual(expectedError, receivedError, file: file, line: line)
                 
             case (.success(let expectedAccount), .success(let receivedAccount)):
-                XCTAssertEqual(expectedAccount, receivedAccount)
+                XCTAssertEqual(expectedAccount, receivedAccount, file: file, line: line)
                 
             default:
-                XCTFail("Expected \(expectedResult) received \(receivedResult) instead")
+                XCTFail("Expected \(expectedResult) received \(receivedResult) instead", file: file, line: line)
             }
             
             exp.fulfill()
