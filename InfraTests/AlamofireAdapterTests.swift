d8e663e07c9b43df6bb7b6d1308a0298fcd5a981
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-15 23:14:32 -0300

    Add Infra layer

---
+++ InfraTests/AlamofireAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  AlamofireAdapterTests.swift
+//  InfraTests
+//
+//  Created by Marcos Barbosa on 15/04/21.
+//
+
+import XCTest
+
+class AlamofireAdapterTests: XCTestCase {
+
+    func test_() {
+       
+    }
+}
