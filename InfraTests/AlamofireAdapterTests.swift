63abe070486e7cfadc542011b6b3cb886f51c597
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-16 00:29:55 -0300

    Move duplicated code to a helper method to improve readability

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -28,27 +28,18 @@ class AlamofireAdapterTests: XCTestCase {
     
     func test_post_should_make_request_with_valid_url_and_method() {
         let url = makeUrl()
-        let sut = makeSut()
-        sut.post(to: url, with: makeValidData())
-        let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
+        
+        testRequestFor(url: url, data: makeValidData()) { request in
             XCTAssertEqual(url, request.url)
             XCTAssertEqual("POST", request.httpMethod)
             XCTAssertNotNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
     
     func test_post_should_make_request_with_no_data() {
-        let sut = makeSut()
-        sut.post(to: makeUrl(), with: nil)
-        let exp = expectation(description: "waiting")
-        UrlProtocolStub.observeRequest { request in
+        testRequestFor(data: nil) { request in
             XCTAssertNil(request.httpBodyStream)
-            exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
     }
 }
 
@@ -61,6 +52,16 @@ extension AlamofireAdapterTests {
         return AlamofireAdapter(session: session)
     }
     
+    func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping(URLRequest) -> Void) {
+        let sut = makeSut()
+        sut.post(to: url, with: data)
+        let exp = expectation(description: "waiting")
+        UrlProtocolStub.observeRequest { request in
+            action(request)
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 class UrlProtocolStub: URLProtocol {
