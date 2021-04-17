3eb29af3b19793a8b0174cda7db77fe9857c3c97
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-17 00:33:21 -0300

    Ensure AlamofireAdapteer completes with error when request completes with error

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -17,8 +17,18 @@ class AlamofireAdapter {
         self.session = session
     }
     
-    func post(to url: URL, with data: Data?) {
-        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).resume()
+    func post(to url: URL, with data: Data?, completion: @escaping(Result<Data?, HttpError>) -> Void) {
+        
+        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).responseData { dataResponse in
+            
+            switch dataResponse.result {
+            case .failure:
+                completion(.failure(.noConnectivity))
+                
+            case .success:
+                break
+            }
+        }
     }
     
 }
@@ -40,6 +50,24 @@ class AlamofireAdapterTests: XCTestCase {
             XCTAssertNil(request.httpBodyStream)
         }
     }
+    
+    func test_post_should_complete_with_error_when_request_completes_with_error() {
+        let sut = makeSut()
+        UrlProtocolStub.simulate(data: nil, response: nil, error: makeError())
+        let exp = expectation(description: "waiting")
+        
+        sut.post(to: makeUrl(), with: makeValidData()) { result in
+            
+            switch result {
+            case .failure(let error):
+                XCTAssertEqual(error, .noConnectivity)
+            case .success:
+                XCTFail("Expected error got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1)
+    }
 }
 
 extension AlamofireAdapterTests {
@@ -55,7 +83,7 @@ extension AlamofireAdapterTests {
     
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping(URLRequest) -> Void) {
         let sut = makeSut()
-        sut.post(to: url, with: data)
+        sut.post(to: url, with: data) { _ in }
         let exp = expectation(description: "waiting")
         UrlProtocolStub.observeRequest { request in
             action(request)
@@ -68,9 +96,18 @@ extension AlamofireAdapterTests {
 class UrlProtocolStub: URLProtocol {
     
     static var emit: ((URLRequest) -> Void)?
+    static var error: Error?
+    static var data: Data?
+    static var response: HTTPURLResponse?
     
     static func observeRequest(completion: @escaping(URLRequest) -> Void) {
         UrlProtocolStub.emit = completion
+    }
+    
+    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
+        UrlProtocolStub.data = data
+        UrlProtocolStub.response = response
+        UrlProtocolStub.error = error
     }
     
     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -83,7 +120,40 @@ class UrlProtocolStub: URLProtocol {
     
     override func startLoading() {
         UrlProtocolStub.emit?(request)
+        
+        if let data = UrlProtocolStub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = UrlProtocolStub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let error = UrlProtocolStub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        }
+        
+        client?.urlProtocolDidFinishLoading(self)
     }
     
     override func stopLoading() {}
 }
+
+/*
+ 
+ data response error
+ 
+ valid
+ ok ok  x
+ x  x   ok
+ 
+ invalid
+ 
+ ok ok ok
+ ok x ok
+ ok x x
+ x ok ok
+ x ok x
+ x x x
+ 
+ */
