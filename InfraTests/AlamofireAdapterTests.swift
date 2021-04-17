a59723c733a6264197bde1470d199d1f814e7586
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-17 00:12:01 -0300

    Add data Extension to covert to json

--- InfraTests/AlamofireAdapterTests.swift
+++ InfraTests/AlamofireAdapterTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import Alamofire
+import Data
 
 class AlamofireAdapter {
     
@@ -17,9 +18,7 @@ class AlamofireAdapter {
     }
     
     func post(to url: URL, with data: Data?) {
-        
-        let json  = data == nil ? nil : try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-        session.request(url, method: .post, parameters: json, encoding: JSONEncoding.default).resume()
+        session.request(url, method: .post, parameters: data?.toJson(), encoding: JSONEncoding.default).resume()
     }
     
 }
