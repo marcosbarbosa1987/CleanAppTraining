a110dc5ae0811cb82ccec39cd0c578160a5b89f2
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-22 23:41:44 -0300

    Move SUT creation to a factory method

--- PresentationTests/SignupPresenterTests.swift
+++ PresentationTests/SignupPresenterTests.swift
@@ -42,8 +42,7 @@ class SignupPresenterTests: XCTestCase {
     
     func test_signUp_should_show_error_message_if_name_is_not_provided() {
         
-        let alertViewSpy = AlertViewSpy()
-        let sut = SignUpPresenter(alertView: alertViewSpy)
+        let (sut, alertViewSpy) = makeSUT()
         let signUpViewModel = SignUpViewModel(email: "any_email@mail.com", password: "any_password", passwordConfirmation: "any_password")
         
         sut.signUp(viewModel: signUpViewModel)
@@ -54,10 +53,15 @@ class SignupPresenterTests: XCTestCase {
 
 extension SignupPresenterTests {
     
+    func makeSUT() -> (sut: SignUpPresenter, alertViewSpy: AlertViewSpy) {
+        let alertViewSpy = AlertViewSpy()
+        let sut = SignUpPresenter(alertView: alertViewSpy)
+        return (sut, alertViewSpy)
+    }
+    
     class AlertViewSpy: AlertView {
-     
         var viewModel: AlertViewModel?
-        
+
         func showMessage(viewModel: AlertViewModel) {
             self.viewModel = viewModel
         }
