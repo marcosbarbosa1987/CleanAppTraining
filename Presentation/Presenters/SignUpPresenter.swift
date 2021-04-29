2e48149f79c31af7d4d1db0254ba322c96086105
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-28 23:36:31 -0300

    Ensure SighUpPresenter shows error message if password confirmation fails

--- Presentation/Presenters/SignUpPresenter.swift
+++ Presentation/Presenters/SignUpPresenter.swift
@@ -30,6 +30,8 @@ public class SignUpPresenter {
             return "O campo Senha é obrigatório"
         } else if viewModel.passwordConfirmation == nil || (viewModel.passwordConfirmation ?? "").isEmpty {
             return "O campo Confirmação de Senha é obrigatório"
+        } else if viewModel.password != viewModel.passwordConfirmation {
+            return "Falha ao confirmar senha"
         }
         return nil
     }
