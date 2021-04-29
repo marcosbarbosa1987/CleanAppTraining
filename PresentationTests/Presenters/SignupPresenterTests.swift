48de9dc547473d413cbfabd355dc70b2ca15aa25
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-04-28 23:33:12 -0300

    Move presentation classes to their own files

--- PresentationTests/Presenters/SignupPresenterTests.swift
+++ PresentationTests/Presenters/SignupPresenterTests.swift
@@ -6,50 +6,7 @@
 //
 
 import XCTest
-
-class SignUpPresenter {
-    
-    private let alertView: AlertView
-    
-    init(alertView: AlertView) {
-        self.alertView = alertView
-    }
-    
-    func signUp(viewModel: SignUpViewModel) {
-        if let message = validate(viewModel: viewModel) {
-            alertView.showMessage(viewModel: AlertViewModel(title: "Falha na validação", message: message))
-        }
-    }
-    
-    private func validate(viewModel: SignUpViewModel) -> String? {
-        if viewModel.name == nil || (viewModel.name ?? "").isEmpty {
-            return "O campo Nome é obrigatório"
-        } else if viewModel.email == nil || (viewModel.email ?? "").isEmpty {
-            return "O campo Email é obrigatório"
-        } else if viewModel.password == nil || (viewModel.password ?? "").isEmpty {
-            return "O campo Senha é obrigatório"
-        } else if viewModel.passwordConfirmation == nil || (viewModel.passwordConfirmation ?? "").isEmpty {
-            return "O campo Confirmação de Senha é obrigatório"
-        }
-        return nil
-    }
-}
-
-protocol AlertView {
-    func showMessage(viewModel: AlertViewModel)
-}
-
-struct AlertViewModel: Equatable {
-    var title: String
-    var message: String
-}
-
-struct SignUpViewModel {
-    var name: String?
-    var email: String?
-    var password: String?
-    var passwordConfirmation: String?
-}
+import Presentation
 
 class SignupPresenterTests: XCTestCase {
     
