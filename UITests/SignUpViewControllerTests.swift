bc7a6f587d471ddc4dccac95760a0b6c977de894
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-05-04 00:16:11 -0300

    Add UI layer

---
+++ UITests/SignUpViewControllerTests.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpViewControllerTests.swift
+//  UITests
+//
+//  Created by Marcos Barbosa on 04/05/21.
+//
+
+import XCTest
+
+class SignUpViewControllerTests: XCTestCase {
+    
+    func test_() {
+        
+    }
+}
