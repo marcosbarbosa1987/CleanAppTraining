0da710b5c07f2d3b1d23c7fdc55aa1309d494633
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-05-29 19:41:03 -0300

    Make showMessage present an AlertViewController

--- UI/Scenes/SignUp/SignUpViewController.swift
+++ UI/Scenes/SignUp/SignUpViewController.swift
@@ -62,6 +62,8 @@ extension SignUpViewController: LoadingView {
 extension SignUpViewController: AlertView {
    
     func showMessage(viewModel: AlertViewModel) {
-        
+        let alert = UIAlertController(title: viewModel.title, message: viewModel.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
