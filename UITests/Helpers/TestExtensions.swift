7b215e8e3859ea49a90ddcd65aaf0be0496952c7
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-05-29 19:38:33 -0300

    Move UIExtension to a separate file

---
+++ UITests/Helpers/TestExtensions.swift
@@ -0,0 +1,23 @@
+//
+//  TestExtensions.swift
+//  UITests
+//
+//  Created by Marcos Barbosa on 29/05/21.
+//
+
+import Foundation
+import UIKit
+
+extension UIControl {
+    func simulate(event: UIControl.Event) {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: event)?.forEach{ action in
+                (target as NSObject).perform(Selector(action))
+            }
+        }
+    }
+    
+    func simulateTap() {
+        simulate(event: .touchUpInside)
+    }
+}
