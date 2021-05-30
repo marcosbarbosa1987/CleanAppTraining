d7225e46a41f994f034a9429f341332a2895a34e
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-05-29 22:05:24 -0300

    Move SUT creation to a factor method, and create test to valid email;

--- ValidationTests/EmailValidatorAdapterTests.swift
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -22,11 +22,25 @@ public final class EmailValidatorAdapter: EmailValidator {
 class EmailValidatorAdapterTests: XCTestCase {
 
     func test_invalid_emails() {
-        let sut = EmailValidatorAdapter()
+        let sut = makeSut()
         XCTAssertFalse(sut.isValid(email: "rr"))
         XCTAssertFalse(sut.isValid(email: "rr@"))
         XCTAssertFalse(sut.isValid(email: "rr@rr"))
         XCTAssertFalse(sut.isValid(email: "rr@rr."))
         XCTAssertFalse(sut.isValid(email: "@rr.com"))
+    }
+    
+    func test_valid_emails() {
+        let sut = makeSut()
+        XCTAssertTrue(sut.isValid(email: "rodrigo@gmail.com"))
+        XCTAssertTrue(sut.isValid(email: "rodrigo@gmail.com.br"))
+        XCTAssertTrue(sut.isValid(email: "rodrigo@hotmail.com"))
+        XCTAssertTrue(sut.isValid(email: "rodrigo@hotmail.com.br"))
+    }
+}
+
+extension EmailValidatorAdapterTests {
+    func makeSut() -> EmailValidatorAdapter {
+        return EmailValidatorAdapter()
     }
 }
