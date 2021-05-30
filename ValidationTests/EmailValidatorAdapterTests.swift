bf0bd953f2d5a7897ab3706e16f326c67c39e51d
Marcos Barbosa <marcosbarbosa1987@live.com>
2021-05-29 21:54:45 -0300

    Add validation target

---
+++ ValidationTests/EmailValidatorAdapterTests.swift
@@ -0,0 +1,15 @@
+//
+//  EmailValidatorAdapterTests.swift
+//  ValidationTests
+//
+//  Created by Marcos Barbosa on 29/05/21.
+//
+
+import XCTest
+
+class EmailValidatorAdapterTests: XCTestCase {
+
+    func test_() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+}
